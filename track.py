f092ac360a899a3a7e955354ff32b4197d6755ab
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-11-04 20:12:33 -0400

    Diagram points

--- track.py
+++ track.py
@@ -2,6 +2,27 @@ from ultralytics import YOLO
 from multiprocessing import Process, Queue
 
 
+DIAGRAM_POINTS = {
+    "left_corner": (145, 130),
+    "right_corner": (1355, 130),
+    "left_tick": (655, 130),
+    "right_tick": (1345, 130),
+    "middle": (1000, 130),
+    "left_paint": {
+        "t_left": (145, 440),
+        "t_right": (490, 440),
+        "b_left": (145, 730),
+        "b_right": (490, 730),
+    },
+    "right_paint": {
+        "t_left": (1510, 440),
+        "t_right": (1855, 440),
+        "b_left": (1510, 730),
+        "b_right": (1855, 730),
+    },
+}
+
+
 def detect(detection_list, model_path, source):
     model = YOLO(model_path)
     detections = model.track(
@@ -27,7 +48,6 @@ def track():
     player_thread.start()
     court_thread.start()
 
-    index = 0
     while (
         player_thread.is_alive()
         or court_thread.is_alive()
@@ -37,13 +57,10 @@ def track():
         if player_detection_queue.empty() and not court_detection_queue.empty():
             continue
 
-        # print(player_detection_queue.qsize())
-
         player_data = player_detection_queue.get()
         court_data = court_detection_queue.get()
         print("player_data: ", player_data)
         print("court_data: ", court_data)
-        index += 1
 
     player_thread.join()
     court_thread.join()
