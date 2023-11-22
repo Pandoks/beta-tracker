96e874a7c3d03856f6263b66b7eb5a0f9fb996ce
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2023-11-22 18:36:40 -0500

    Define court different paint detection

--- track.py
+++ track.py
@@ -28,6 +28,8 @@ DIAGRAM_POINTS = {
 COURT_CLASSES = ["corner", "middle", "paint", "tick"]
 PLAYER_CLASSES = ["Players", "Referees"]
 
+VIDEO_HEIGHT = 0
+
 
 def center_court(coords):
     x1, y1, x2, y2 = coords
@@ -62,33 +64,45 @@ def parse_player(data):
         coords = (label[0], label[1], label[2], label[3])
         conf = label[-2].item()
         label_class = PLAYER_CLASSES[int(label[-1])]
-        id = None
+        id = None  # tracking id
         if len(label) == 7:
             id = int(label[-3])
         labels[label_class].append((center_player(coords), conf, id))
     return labels
 
 
-def define_paint(corners):
-    if len(corners) != 4:
+def court_direction_paint(paints):
+    left_threshold = 0.7 * VIDEO_HEIGHT
+    right_threshold = 0.4 * VIDEO_HEIGHT
+
+
+def define_court(features):
+    # check for paint corners:
+    paint_points = features["paint"]
+    if len(paint_points) < 4:
+        return None
+    elif len(paint_points) > 4:
         return None
 
-    sorted_x_corners = sorted(corners, key=lambda x: x[0])
+    sorted_x_corners = sorted(paint_points, key=lambda x: x[0])
     left_corners = sorted_x_corners[:2]
     right_corners = sorted_x_corners[2:]
 
     sorted_left_y_corners = sorted(left_corners, key=lambda x: x[1])
     sorted_right_y_corners = sorted(right_corners, key=lambda x: x[1])
+
     return {
-        "top_left": sorted_left_y_corners[0],
-        "top_right": sorted_right_y_corners[0],
-        "bottom_left": sorted_left_y_corners[1],
-        "bottom_right": sorted_right_y_corners[1],
+        "top_left": sorted_left_y_corners[0][0],
+        "top_right": sorted_right_y_corners[0][0],
+        "bottom_left": sorted_left_y_corners[1][0],
+        "bottom_right": sorted_right_y_corners[1][0],
     }
 
 
 def homography(labels, threshold=0):
-    paint = define_paint([label[0] for label in labels["paint"]])
+    # paint = define_court([label[0] for label in labels["paint"]])
+    paint = define_court(labels)
+    print(paint)
     if not paint:
         return None
 
@@ -168,15 +182,17 @@ def track():
         court_data = court_detection_list.pop(0)
         player_data = player_detection_list.pop(0)
         court_data = parse_court(court_data)
+        print("----------COURT_DATA-----------")
+        print(court_data)
         player_data = parse_player(player_data)
         homography_matrix = homography(court_data)
         _, frame = video.read()
+        VIDEO_WIDTH = frame.shape[1]
         if homography_matrix is None:
             continue
 
         transformed_points = transform_points(player_data["Players"], homography_matrix)
         image = cv2.imread("data/court.png")
-        frame_width = frame.shape[1]
         font = cv2.FONT_HERSHEY_SIMPLEX
         for x, y in transformed_points:
             cv2.circle(image, (x, y), radius=5, color=(0, 0, 0), thickness=-1)
@@ -194,7 +210,7 @@ def track():
             cv2.putText(frame, f"({x},{y})", (x, y), font, 0.4, (0, 0, 0), 1)
 
         image = cv2.resize(
-            image, (frame_width, int(image.shape[0] * (frame_width / image.shape[1])))
+            image, (VIDEO_WIDTH, int(image.shape[0] * (VIDEO_WIDTH / image.shape[1])))
         )
 
         combined = cv2.vconcat([frame, image])
